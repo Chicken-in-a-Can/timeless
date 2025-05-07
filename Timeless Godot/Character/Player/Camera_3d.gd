b54c6a550c932b94a1112135dceaffdb0949767b
Evan Larson <evantylerlarson@gmail.com>
2025-05-07 11:38:58 -0500

    Added Godot
    (Using Godot 4.3)
    I also implemented a quick character and mouse interaction.
    The player does not yet move in direction of camera

---
+++ Timeless Godot/Character/Player/Camera_3d.gd
@@ -0,0 +1,26 @@
+class_name Camera
+extends Camera3D
+
+static var captured: bool = true 
+#this should be referenced when taking user input, determines whether or not the mouse is captured
+
+func _ready() -> void:
+	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if(captured):
+		if(event is InputEventMouseMotion):
+				rotation.y -= event.relative.x * .005
+		elif(event is InputEventKey):
+			if(event.keycode == 4194305): #escape
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+				captured = false
+	elif(event is InputEventMouseButton):
+		if(event.button_index == MOUSE_BUTTON_LEFT):
+			Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
+			captured = true
